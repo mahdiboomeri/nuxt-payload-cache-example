7bedf4440bd6153a2ae5e5ea19212bdc318e2745
Mahdi Boomeri <mahdi.b2or@gmail.com>
2022-11-21 20:56:24 +0330

    init

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLink href="/test">Go to page</NuxtLink>
+
+  <br />
+
+  Hello
+</template>
+
+<script setup lang="ts">
+const { data } = await useAsyncData("posts", () => $fetch("/api/hello"));
+</script>
---
+++ pages/test.vue
@@ -0,0 +1,9 @@
+<template>
+  <button @click="log('posts')">Log the data</button>
+</template>
+
+<script setup lang="ts">
+const log = (key: string) => {
+  console.log(useNuxtApp().payload.data[key]);
+};
+</script>
